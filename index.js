04be44c569cb33fba303ad5152064d90339be04a
James Quiambao <james.quiambao@yahoo.ca>
2016-11-29 00:38:34 -0500

    Starting chatbot

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express'),
+	bodyparser = require('bodyparser'),
+	request = require('request'); 
+
+var app = express();
+
+app.set('port', process.env.PORT || 5000); 
+
+//Process the body of requests
+app.use(bodyparser.urlencoded{extended: false});
+app.use(bodyparser.json());
+
+//Routes
+
+app.get('/', function(req,res){
+	res.send("Hi, Im a chatbot"); 
+})
+
+//Facebook
+app.get('/webhook/', function(req, res){
+	if (req.query['hub.verify_token'] === 'chatbot'){
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Wrong Token'); 
+})
+
+app.listen(app.get('port'), function(){
+	console.log("running: port")
+})
+
